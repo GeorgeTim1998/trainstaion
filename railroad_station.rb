f6a44758934669c51a75f81fbf1bf13809914662
GeorgeTim1998 <92552857+GeorgeTim1998@users.noreply.github.com>
2021-11-19 20:23:37 +0300

    Add files via upload

--- railroad_station.rb
+++ railroad_station.rb
@@ -7,95 +7,116 @@ class Station
   end
 
   def arrive(train)
-    self.trains << train
+    trains << train
   end
 
   def depart(train)
-    train.accept_route
     trains.delete(train)
   end
 
-  def trains_type
-    trains.sort_by{|train| train.type}
+  def trains_by_type(type)
+    trains.select { |train| train.type == type }
   end
 
-  def trains_type_amount
-    trains_amount = trains.count { |train| train.type == 'cargo' }
-
-    [trains_amount, trains.length - trains_amount]
+  def trains_by_type_amount(type)
+    puts "Trains of '#{type}' type is:"
+    trains_by_type(type).count
   end
 end
 
 class Route
   attr_reader :stations
-  
-  def initialize(departure, destination)
-    @departure = departure
-    @destination = destination
-    @stations = []
 
+  def initialize(departure, destination)
     @stations = [departure, destination]
   end
 
   def add_waypoint(waypoint)
-    stations_amount = self.stations.length
-    self.stations.insert(stations_amount - 1, waypoint)
+    stations.insert(-2, waypoint)
   end
 
   def delete_waypoint(waypoint)
-    if (waypoint != @departure) && (waypoint != @destination) 
-      self.stations.delete(waypoint) 
-    end
+    stations.delete(waypoint) if (waypoint != @departure) && (waypoint != @destination)
   end
 
   def show_route
-    puts self.stations
+    puts stations
   end
 end
 
 class Train
-  attr_reader :type 
+  attr_reader :type
   attr_accessor :speed, :car_amount, :curr_station
 
   def initialize(number, type, car_amount)
     @number = number
     @type = type
     @car_amount = car_amount
+    @route = nil
   end
 
   def stop
     self.speed = 0
   end
 
-  def change_car_amount(what_to_do)
-    self.stop
-    if what_to_do == 'increment'
-      self.car_amount += 1
-    end
+  def add_car
+    stop
+    self.car_amount += 1
+  end
 
-    if what_to_do == 'decrement'
-      self.car_amount -= 1
+  def delete_car
+    stop
+    if self.car_amount.zero?
+      puts "There is already zero cars! Can't delete cars futher!"
+    else
+      self.car_amount += -1
     end
-    
   end
 
   def accept_route(route)
-    @route = route.stations
-    self.curr_station = @route[0]
+    @route = route
+    self.curr_station = @route.stations[0]
   end
 
   def forward
-    index = @route.find_index(self.curr_station)
-    self.curr_station = @route[index + 1]
+    index = @route.stations.find_index(curr_station)
+
+    case index
+    when @route.stations.length - 1
+      puts "Can't go further!"
+    else
+      self.curr_station = @route.stations[index + 1]
+    end
   end
 
   def backward
-    index = @route.find_index(self.curr_station)
-    self.curr_station = @route[index - 1]
+    index = @route.stations.find_index(curr_station)
+
+    case index
+    when 0
+      puts "Can't go further!"
+    else
+      self.curr_station = @route.stations[index - 1]
+    end
   end
 
-  def show_route
-    index = @route.find_index(self.curr_station)
-    @route[index - 1, 3]
+  def prev_station
+    index = @route.stations.find_index(curr_station)
+
+    if index.zero?
+      puts "There is no previous station. The train is at the departure '#{@route.stations[0]}'!"
+    else
+      @route.stations[index - 1]
+    end
+  end
+
+  def next_station
+    index = @route.stations.find_index(curr_station)
+
+    if index == @route.stations.length - 1
+      puts "There is no next station. The train is at the destination '#{@route.stations[-1]}'!"
+    else
+      @route.stations[index + 1]
+    end
   end
 end
