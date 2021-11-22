7644a701453353bb905fb6ba511648a463a995cc
georgetim1998 <georgetim1998@gmail.com>
2021-11-22 23:23:30 +0300

    Setup for the task

---
+++ station.rb
@@ -0,0 +1,25 @@
+class Station
+  attr_reader :trains
+
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
+
+  def arrive(train)
+    trains << train
+  end
+
+  def depart(train)
+    trains.delete(train)
+  end
+
+  def trains_by_type(type)
+    trains.select { |train| train.type == type }
+  end
+
+  def trains_by_type_amount(type)
+    puts "Trains of '#{type}' type is:"
+    trains_by_type(type).count
+  end
+end
