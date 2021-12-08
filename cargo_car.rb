0f1ccc1bf68df7d59833ed65545aa759a4f62f4b
georgetim1998 <georgetim1998@gmail.com>
2021-12-08 14:39:20 +0300

    added required by task functionalyty

--- cargo_car.rb
+++ cargo_car.rb
@@ -3,9 +3,30 @@ require_relative 'car'
 class CargoCar < Car
 # с этими методами взаимодействует только класс Interface. От пользователя надо отгородиться
 
-  def initialize
+  def initialize(volume)
     @type = DEFAULT_TYPE
+    @volume = volume
+    @occupied_volume = 0 
+    validate!
   end
 
+  def take_seat
+    @occupied_volume += 1 if @occupied_volume < @volume
+  end
+  
+  def taken_seats
+    @occupied_volume
+  end
+  
+  def free_seats
+    @volume - @occupied_volume
+  end
+  
+  protected
+  
   DEFAULT_TYPE = 'cargo'
+
+  def validate!
+    raise 'Incorrect volume. Must be positive integer!' unless @volume.is_a?(Integer) && @volume.positive?
+  end
 end
--- passenger_car.rb
+++ passenger_car.rb
@@ -1,8 +1,32 @@
+require_relative 'car'
 class PassCar < Car
   # с этими методами взаимодействует только класс Interface. От пользователя надо отгородиться
-  def initialize
+  attr_reader :seats_amount
+
+  def initialize(volume)
     @type = DEFAULT_TYPE
+    @volume = volume
+    @occupied_seats = 0 
+    validate!
   end
 
+  def take_seat
+    @occupied_seats += 1 if @occupied_seats < @volume
+  end
+
+  def taken_seats
+    @occupied_seats
+  end
+
+  def free_seats
+    @volume - @occupied_seats
+  end
+  
+  protected
+  
   DEFAULT_TYPE = 'passenger'
+
+  def validate!
+    raise 'Incorrect seats amount. Must be positive integer!' unless @volume.is_a?(Integer) && @volume.positive?
+  end
 end
